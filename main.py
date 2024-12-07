0d9d294b314e3540950c95e33a3c0add13e585be
Sumitpatel080 <patrlsumit93@gmail.com>
2024-12-07 21:15:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from bot import Bot
 
 Bot().run()
+from telegram.ext import Updater, CommandHandler
+from commands.admin import setchannel
+
+# Initialize bot
+updater = Updater("YOUR_BOT_TOKEN")
+dispatcher = updater.dispatcher
+
+# Register admin commands
+dispatcher.add_handler(CommandHandler("setchannel", setchannel))
+
+# Start the bot
+updater.start_polling()
+updater.idle()
